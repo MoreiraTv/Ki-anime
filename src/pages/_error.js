7b8c9e8a2b559c48e21cb7bebec7ff18b0fe3571
Matheus Moreira <matheusmoreira08@gmail.com>
2021-12-27 22:35:23 -0300

    corrigindo error6

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,13 +1,16 @@
-import Error from 'next/error';
-
-
-function Page({ statusCode }) {
-  return <Error statusCode={statusCode}></Error>;
+function Error({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
 }
 
-Page.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
 
-export default Page;
+export default Error
