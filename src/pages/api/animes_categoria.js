ac1f977ebb3fa2d24b9eb8d0d2bdf9a2209d1eb6
Matheus Moreira <matheusmoreira08@gmail.com>
2021-12-27 23:59:32 -0300

    resolve bugs check rename arquivo

--- src/pages/api/animes_categoria.js
+++ src/pages/api/animes_categoria.js
@@ -12,21 +12,20 @@ async function animesCatergoria(resquest,response) {
   let lisCatArray = Array.from(listCat.data.listCat.data)
 
 
-  // for(let x = 0; x < lisCatArray.length; x++){
-  //   let catAtual = lisCatArray[x].attributes.title
-  //   const resp = await api.get(`anime?filter[categories]=${catAtual}$&page[limit]=10`)
-  //   const response = resp.data
-  //   const y = Array.from(response)
-  //   y.push({categoria: catAtual , response});
-  //   listAnimePorCat.push(y)
-  // }
+  for(let x = 0; x < lisCatArray.length; x++){
+    let catAtual = lisCatArray[x].attributes.title
+    const resp = await api.get(`anime?filter[categories]=${catAtual}$&page[limit]=10`)
+    const response = resp.data
+    const y = Array.from(response)
+    y.push({categoria: catAtual , response});
+    listAnimePorCat.push(y)
+  }
   listAnimePorCat = JSON.stringify(listAnimePorCat)
   console.log(typeof  listAnimePorCat)
   response.setHeader('Cache-Control', 's-maxage=0', 'stale-while-revalidate')
-  // response.json({
-  //   listAnimePorCat
-  // })
-  response.json("teste")
+  response.json({
+    listAnimePorCat
+  })
   
 }
 
